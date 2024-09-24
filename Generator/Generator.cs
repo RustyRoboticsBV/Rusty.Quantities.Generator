983205584cfdc53a56caf0baeec0ae7f02a3b9e0
RustyRoboticsBV <rusty.robotics.bv@gmail.com>
2024-09-24 23:33:31 +0200

    Going to refactor generator architecture.

---
+++ Generator/Generator.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace Generator
+{
+    public abstract class Generator
+    {
+        public static string Indent => new(' ', 8);
+    }
+}
---
+++ Generator/IGenerator.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace Generator
+{
+    public interface IGeneratorBiNas
+    {
+    }
+
+    public interface IGenerator : IGeneratorBiNas
+    {
+        public string Generate();
+    }
+
+    public interface IGenerator<T> : IGeneratorBiNas
+    {
+        public string Generate(T arg);
+    }
+
+    public interface IGenerator<T, U> : IGeneratorBiNas
+    {
+        public string Generate(T arg1, U arg2);
+    }
+
+    public interface IGenerator<T, U, V> : IGeneratorBiNas
+    {
+        public string Generate(T arg1, U arg2, V arg3);
+    }
+}
