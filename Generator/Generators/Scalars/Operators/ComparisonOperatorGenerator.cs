037261eac1a262e508ea5ae8d59b84e415fd5edb
RustyRoboticsBV <rusty.robotics.bv@gmail.com>
2024-09-25 16:41:31 +0200

    Added explicit comparison operators for the byte, sbyte, short, ushort, int, uint, long, ulong, float and double types.

--- Generator/Generators/Scalars/Operators/ComparisonOperatorGenerator.cs
+++ Generator/Generators/Scalars/Operators/ComparisonOperatorGenerator.cs
@@ -8,30 +8,66 @@ namespace Generators.Scalars
     public class ComparisonOperatorGenerator : Generator
     {
         /* Public methods. */
-        public static string Generate(string type1, string type2, string operatorName, string term1, string term2, string summary = null)
+        public static string Generate(string type1, string type2, string opName, string term1, string term2, string summary = null)
         {
             return OperatorGenerator.Generate(
                 null,
                 "bool",
-                operatorName,
+                opName,
                 $"{type1} a, {type2} b",
-                $"return {term1} {operatorName} {term2};",
+                $"return {term1} {opName} {term2};",
                 summary);
-        }
-
-        public static string Generate(string className, string operatorName)
-        {
-            return Generate(className, className, operatorName, "a.value", "b.value");
         }
 
         public static string Generate(string className)
         {
-            return Generate(className, "==")
-                + "\n" + Generate(className, "!=")
-                + "\n" + Generate(className, ">")
-                + "\n" + Generate(className, "<")
-                + "\n" + Generate(className, ">=")
-                + "\n" + Generate(className, "<=");
+            return GenerateAll(className, "==")
+                + "\n" + GenerateAll(className, "!=")
+                + "\n" + GenerateAll(className, ">")
+                + "\n" + GenerateAll(className, "<")
+                + "\n" + GenerateAll(className, ">=")
+                + "\n" + GenerateAll(className, "<=");
+        }
+
+        /* Private methods. */
+        private static string GenerateCC(string className, string opName)
+        {
+            return Generate(className, className, opName, "a.value", "b.value");
+        }
+
+        private static string GenerateTC(string typeName, string opName, string className)
+        {
+            return Generate(typeName, className, opName, "a", "b.value");
+        }
+
+        private static string GenerateCT(string typeName, string opName, string className)
+        {
+            return Generate(typeName, className, opName, "a.value", "b");
+        }
+
+        private static string GenerateAll(string className, string opName)
+        {
+            return GenerateTC("byte", opName, className)
+                + "\n" + GenerateTC("ushort", opName, className)
+                + "\n" + GenerateTC("uint", opName, className)
+                + "\n" + GenerateTC("ulong", opName, className)
+                + "\n" + GenerateTC("sbyte", opName, className)
+                + "\n" + GenerateTC("short", opName, className)
+                + "\n" + GenerateTC("int", opName, className)
+                + "\n" + GenerateTC("long", opName, className)
+                + "\n" + GenerateTC("float", opName, className)
+                + "\n" + GenerateTC("double", opName, className)
+                + "\n" + GenerateCT(className, opName, "byte")
+                + "\n" + GenerateCT(className, opName, "ushort")
+                + "\n" + GenerateCT(className, opName, "uint")
+                + "\n" + GenerateCT(className, opName, "ulong")
+                + "\n" + GenerateCT(className, opName, "sbyte")
+                + "\n" + GenerateCT(className, opName, "short")
+                + "\n" + GenerateCT(className, opName, "int")
+                + "\n" + GenerateCT(className, opName, "long")
+                + "\n" + GenerateCT(className, opName, "float")
+                + "\n" + GenerateCT(className, opName, "double")
+                + "\n" + GenerateCC(className, opName);
         }
     }
 }
--- Generator/Generators/Scalars/Operators/MathOperatorGenerator.cs
+++ Generator/Generators/Scalars/Operators/MathOperatorGenerator.cs
@@ -43,68 +43,60 @@ namespace Generators.Scalars
         }
 
         /* Private methods. */
-        private static string GenerateCC(string className, string operatorName)
+        private static string GenerateCC(string className, string opName)
         {
             return GenerateBinary(className,
-                operatorName,
+                opName,
                 className, className,
-                $"return new {className}({ToDouble("a", true)} {operatorName} {ToDouble("b", true)});");
+                $"return new {className}(a.value {opName} b.value);");
         }
 
-        private static string GenerateTC(string typeName, string operatorName, string className)
+        private static string GenerateTC(string typeName, string opName, string className)
         {
             return GenerateBinary(className,
-                operatorName,
+                opName,
                 typeName, className,
-                $"return new {className}({ToDouble("a", false)} {operatorName} {ToDouble("b", true)});");
+                $"return new {className}(a {opName} b.value);");
         }
 
-        private static string GenerateCT(string className, string operatorName, string typeName)
+        private static string GenerateCT(string className, string opName, string typeName)
         {
             return GenerateBinary(className,
-                operatorName,
+                opName,
                 className, typeName,
-                $"return new {className}({ToDouble("a", true)} {operatorName} {ToDouble("b", false)});");
+                $"return new {className}(a.value {opName} b);");
         }
 
-        private static string GenerateUnary(string className, string operatorSymbol)
+        private static string GenerateUnary(string className, string opName)
         {
             return GenerateUnary(className,
-                operatorSymbol,
-                $"return new {className}({operatorSymbol}value.value);");
+                opName,
+                $"return new {className}({opName}value.value);");
         }
 
-        private static string GenerateAll(string className, string operatorSymbol)
+        private static string GenerateAll(string className, string opName)
         {
-            return GenerateTC("byte", operatorSymbol, className)
-                + "\n" + GenerateTC("ushort", operatorSymbol, className)
-                + "\n" + GenerateTC("uint", operatorSymbol, className)
-                + "\n" + GenerateTC("ulong", operatorSymbol, className)
-                + "\n" + GenerateTC("sbyte", operatorSymbol, className)
-                + "\n" + GenerateTC("short", operatorSymbol, className)
-                + "\n" + GenerateTC("int", operatorSymbol, className)
-                + "\n" + GenerateTC("long", operatorSymbol, className)
-                + "\n" + GenerateTC("float", operatorSymbol, className)
-                + "\n" + GenerateTC("double", operatorSymbol, className)
-                + "\n" + GenerateCT(className, operatorSymbol, "byte")
-                + "\n" + GenerateCT(className, operatorSymbol, "ushort")
-                + "\n" + GenerateCT(className, operatorSymbol, "uint")
-                + "\n" + GenerateCT(className, operatorSymbol, "ulong")
-                + "\n" + GenerateCT(className, operatorSymbol, "sbyte")
-                + "\n" + GenerateCT(className, operatorSymbol, "short")
-                + "\n" + GenerateCT(className, operatorSymbol, "int")
-                + "\n" + GenerateCT(className, operatorSymbol, "long")
-                + "\n" + GenerateCT(className, operatorSymbol, "float")
-                + "\n" + GenerateCT(className, operatorSymbol, "double")
-                + "\n" + GenerateCC(className, operatorSymbol);
-        }
-
-        private static string ToDouble(string id, bool isMainClass)
-        {
-            if (isMainClass)
-                return $"{id}.value";
-            else
-                return id;
+            return GenerateTC("byte", opName, className)
+                + "\n" + GenerateTC("ushort", opName, className)
+                + "\n" + GenerateTC("uint", opName, className)
+                + "\n" + GenerateTC("ulong", opName, className)
+                + "\n" + GenerateTC("sbyte", opName, className)
+                + "\n" + GenerateTC("short", opName, className)
+                + "\n" + GenerateTC("int", opName, className)
+                + "\n" + GenerateTC("long", opName, className)
+                + "\n" + GenerateTC("float", opName, className)
+                + "\n" + GenerateTC("double", opName, className)
+                + "\n" + GenerateCT(className, opName, "byte")
+                + "\n" + GenerateCT(className, opName, "ushort")
+                + "\n" + GenerateCT(className, opName, "uint")
+                + "\n" + GenerateCT(className, opName, "ulong")
+                + "\n" + GenerateCT(className, opName, "sbyte")
+                + "\n" + GenerateCT(className, opName, "short")
+                + "\n" + GenerateCT(className, opName, "int")
+                + "\n" + GenerateCT(className, opName, "long")
+                + "\n" + GenerateCT(className, opName, "float")
+                + "\n" + GenerateCT(className, opName, "double")
+                + "\n" + GenerateCC(className, opName);
         }
     }
 }
