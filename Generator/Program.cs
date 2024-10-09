f99dc3cb84d8e9d6daf2ffbd27bcfd8ff65ddc08
RustyRoboticsBV <rusty.robotics.bv@gmail.com>
2024-10-09 04:06:51 +0200

    Added tests, and fixed the faulty formulas that were revealed during testing.

--- Generator/Program.cs
+++ Generator/Program.cs
@@ -10,12 +10,12 @@ FormulaParameter acceleration = new FormulaParameter('a', "acceleration", Quanti
 
 // Define formula sets.
 FormulaSet tsv = new FormulaSet("V=s/t", "t=s/V", "s=V*t", constantSpeed, distance, time);
-FormulaSet tsuw = new FormulaSet("s=1/2*(v-u)*t", "t=2*s/(v-u)", "v=u+2*s/t", "u=v-2*s/t", startSpeed, endSpeed, distance, time);
-FormulaSet tuwa = new FormulaSet("v=u*t+1/2*a*POW2(t)", "u=v/t-1/2*a*t", "a=2*(v-t*u)/POW2(t)", "t=(SQRT(2*a*v+POW2(u))-u)/a", startSpeed, endSpeed, acceleration, time);
-FormulaSet suwa = new FormulaSet("v=SQRT(POW2(u)+2*a*s)", "u=SQRT(POW2(v)-2*a*s)", "a=(POW2(v)-POW2(u))/(2*s)", "s=(POW2(v)-POW2(u))/(2*a)", startSpeed, endSpeed, acceleration, distance);
-FormulaSet tsua = new FormulaSet("s=u*t+1/2*a*POW2(t)", "a=2*(s+u*t)/POW2(t)", "u=s/t-1/2*a*t", "t=(SQRT(2*a*s+POW2(u))-u)/a", startSpeed, acceleration, distance, time);
-FormulaSet tswa = new FormulaSet("s=v*t-1/2*a*POW2(t)", "a=UMIN2*(s-v*t)/POW2(t)", "v=s/t+1/2*a*t", "t=(SQRT(UMIN2*a*s+POW2(v))+v)/a", endSpeed, acceleration, distance, time);
-FormulaSet[] formulas = new FormulaSet[] { tsv, tsuw, tuwa, suwa, tsua, tswa };
+FormulaSet tsuv = new FormulaSet("s=1/2*(v+u)*t", "v=2*s/t-u", "u=2*s/t-v", "t=2*s/(v+u)", startSpeed, endSpeed, distance, time);
+FormulaSet tuva = new FormulaSet("v=u+a*t", "u=v-a*t", "a=(v-u)/t", "t=(v-u)/a", startSpeed, endSpeed, acceleration, time);
+FormulaSet suva = new FormulaSet("v=SQRT(POW2(u)+2*a*s)", "u=SQRT(POW2(v)-2*a*s)", "a=(POW2(v)-POW2(u))/(2*s)", "s=(POW2(v)-POW2(u))/(2*a)", startSpeed, endSpeed, acceleration, distance);
+FormulaSet tsua = new FormulaSet("s=u*t+1/2*a*POW2(t)", "u=s/t-1/2*a*t", "a=2*s/POW2(t)-2*u/t", "t=(SQRT(2*a*s+POW2(u))-u)/a", startSpeed, acceleration, distance, time);
+FormulaSet tsva = new FormulaSet("s=v*t-1/2*a*POW2(t)", "v=s/t+1/2*a*t", "a=UMIN2*(s-v*t)/POW2(t)", "t=(v-SQRT(POW2(v)-2*a*s))/a", endSpeed, acceleration, distance, time);
+FormulaSet[] formulas = new FormulaSet[] { tsv, tsuv, tuva, suva, tsua, tsva };
 
 // Generate quantity types.
 CSFile.Generate(new Time(formulas));
