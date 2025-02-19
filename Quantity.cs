768bf1cf7262ec656f79b9742c63adf14722ed9f
RustyRoboticsBV <rusty.robotics.bv@gmail.com>
2025-02-19 04:22:35 +0100

    Added properties and override & interface methods.

--- Quantity.cs
+++ Quantity.cs
@@ -1,233 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
-using CSharpGenerator;
+﻿using CSharpGenerator;
 
 namespace Rusty.Quantities.Generator
 {
-    public static class Types
-    {
-        public static HashSet<string> QuantityTypes => new HashSet<string>
-        {
-            "Time", "Distance", "Speed", "Acceleration", "Angle"
-        };
-
-        public static HashSet<string> Byte1Types => new HashSet<string>
-        {
-            "sbyte", "byte"
-        };
-
-        public static HashSet<string> Byte2Types => new HashSet<string>
-        {
-            "short", "ushort"
-        };
-
-        public static HashSet<string> Byte4Types => new HashSet<string>
-        {
-            "int", "uint", "float",
-        };
-
-        public static HashSet<string> Byte8Types => new HashSet<string>
-        {
-            "long", "ulong", "double"
-        };
-
-        public static HashSet<string> Byte16Types => new HashSet<string>
-        {
-            "decimal"
-        };
-
-        public static HashSet<string> IntTypes => new HashSet<string>
-        {
-            "sbyte", "byte", "short", "ushort", "int", "uint", "long", "ulong"
-        };
-
-        public static HashSet<string> FloatTypes => new HashSet<string>
-        {
-            "float", "double", "decimal"
-        };
-
-        public static HashSet<string> SignedTypes => new HashSet<string>
-        {
-            "sbyte", "short", "int", "long", "float", "double", "decimal"
-        };
-
-        public static HashSet<string> UnsignedTypes => new HashSet<string>
-        {
-            "byte", "ushort", "uint", "ulong", "char"
-        };
-
-        public static HashSet<string> PrimitiveTypes => new HashSet<string>
-        {
-            "bool",
-            "sbyte", "short", "int", "long",
-            "byte", "ushort", "uint", "ulong",
-            "float", "double", "decimal",
-            "char", "string"
-        };
-
-        public static HashSet<string> ScalarTypes => new HashSet<string>
-        {
-            "Time", "Distance", "Speed", "Acceleration"
-        };
-
-        public static HashSet<string> Vector2Types => new HashSet<string>
-        {
-            "Distance2D", "Velocity2D", "Acceleration2D"
-        };
-
-        public static HashSet<string> Vector3Types => new HashSet<string>
-        {
-            "Distance3D", "Velocity3D", "Acceleration3D"
-        };
-
-        public static Dictionary<string, string> Adjectives => new Dictionary<string, string>
-        {
-            { "bool", "a" },
-            { "sbyte", "an" },
-            { "byte", "a" },
-            { "short", "a" },
-            { "ushort", "an" },
-            { "int", "an" },
-            { "uint", "an" },
-            { "long", "a" },
-            { "ulong", "an" },
-            { "float", "a" },
-            { "double", "a" },
-            { "decimal", "a" },
-            { "char", "a" },
-            { "string", "a" },
-            { "Time", "a" },
-            { "Distance", "a" },
-            { "Speed", "a" },
-            { "Acceleration", "an" },
-            { "Distance2D", "a" },
-            { "Speed2D", "a" },
-            { "Acceleration2D", "an" },
-            { "Distance3D", "a" },
-            { "Speed3D", "a" },
-            { "Acceleration3D", "an" }
-        };
-
-        public static bool MustCast(string type1, string type2)
-        {
-            int size1 = GetSize(type1);
-            int size2 = GetSize(type2);
-            return FloatTypes.Contains(type1) && IntTypes.Contains(type2)
-                || SignedTypes.Contains(type1) && UnsignedTypes.Contains(type2)
-                || UnsignedTypes.Contains(type1) && SignedTypes.Contains(type2)
-                || size1 > size2
-                || size2 == 16 && size1 != 16 && FloatTypes.Contains(type1);
-        }
-
-        public static string Convert(string value, string fromType, string toType)
-        {
-            if (toType == "bool")
-            {
-                if (PrimitiveTypes.Contains(fromType))
-                    return $"{value} != 0.0";
-
-                if (ScalarTypes.Contains(fromType))
-                    return $"{value} != {fromType}.Zero";
-
-                if (Vector2Types.Contains(fromType))
-                    return $"{value} != {fromType}.Zero";
-
-                if (Vector3Types.Contains(fromType))
-                    return $"{value} != {fromType}.Zero";
-            }
-
-            if (toType == "string" && fromType != "string")
-                return $"{value}.ToString()";
-
-            if (PrimitiveTypes.Contains(fromType))
-            {
-                if (PrimitiveTypes.Contains(toType))
-                {
-                    if (MustCast(fromType, toType))
-                        return $"({toType}){value}";
-                    else
-                        return fromType;
-                }
-
-                if (ScalarTypes.Contains(toType))
-                {
-                    if (MustCast(fromType, "double"))
-                        return $"new {toType}((double){value})";
-                    else
-                        return $"new {toType}({value})";
-                }
-
-                if (Vector2Types.Contains(toType))
-                {
-                    if (MustCast(fromType, "double"))
-                        return $"new {toType}((double){value}, 0.0)";
-                    else
-                        return $"new {toType}({value}, 0.0)";
-                }
-
-                if (Vector3Types.Contains(toType))
-                {
-                    if (MustCast(fromType, "double"))
-                        return $"new {toType}((double){value}, 0.0, 0.0)";
-                    else
-                        return $"new {toType}({value}, 0.0, 0.0)";
-                }
-            }
-
-            if (ScalarTypes.Contains(fromType))
-            {
-                if (PrimitiveTypes.Contains(toType))
-                {
-                    if (MustCast("double", toType))
-                        return $"({toType}){value}.value";
-                    else
-                        return $"{value}.value";
-                }
-
-                if (ScalarTypes.Contains(toType))
-                {
-                    if (MustCast("double", toType))
-                        return $"new {toType}((double){value}.value)";
-                    else
-                        return $"new {toType}({value}.value)";
-                }
-
-                if (Vector2Types.Contains(toType))
-                {
-                    if (MustCast("double", toType))
-                        return $"new {toType}((double){value}.value, 0.0)";
-                    else
-                        return $"new {toType}({value}.value, 0.0)";
-                }
-
-                if (Vector3Types.Contains(toType))
-                {
-                    if (MustCast("double", toType))
-                        return $"new {toType}((double){value}.value, 0.0, 0.0)";
-                    else
-                        return $"new {toType}({value}.value, 0.0, 0.0)";
-                }
-            }
-
-            return value;
-        }
-
-        public static int GetSize(string type)
-        {
-            if (Byte1Types.Contains(type))
-                return 1;
-            if (Byte2Types.Contains(type))
-                return 2;
-            if (Byte4Types.Contains(type))
-                return 4;
-            if (Byte8Types.Contains(type))
-                return 8;
-            if (Byte16Types.Contains(type))
-                return 16;
-            return 0;
-        }
-    }
-
+    /// <summary>
+    /// A quantity struct.
+    /// </summary>
     public class Quantity : Struct
     {
         /* Public properties. */
@@ -257,10 +34,10 @@ namespace Rusty.Quantities.Generator
                 Title = "Public properties.",
                 Members = new IStructMember[]
                 {
-                    GetProperty("Zero", Types.Convert("0.0", "double", Name), $"A {LowercaseName} object with the value 0."),
-                    GetProperty("One", Types.Convert("1.0", "double", Name), $"A {LowercaseName} object with the value 1."),
-                    GetProperty("Pi", Types.Convert("Math.PI", "double", Name), $"A {LowercaseName} object with the value π."),
-                    GetProperty("TwoPi", Types.Convert("2.0 * Math.PI", "double", Name), $"A {LowercaseName} object with the value 2π."),
+                    GetProperty("Zero", Types.Convert("0.0", "double", Name), $"A {Name} object with the value 0."),
+                    GetProperty("One", Types.Convert("1.0", "double", Name), $"A {Name} object with the value 1."),
+                    GetProperty("Pi", Types.Convert("Math.PI", "double", Name), $"A {Name} object with the value π."),
+                    GetProperty("TwoPi", Types.Convert("2.0 * Math.PI", "double", Name), $"A {Name} object with the value 2π."),
                     new Empty()
                 }
             };
@@ -318,7 +95,11 @@ namespace Rusty.Quantities.Generator
                 Title = "Public methods.",
                 Members = new IStructMember[]
                 {
-                    GetToString()
+                    GetToString(), new Empty(),
+                    GetEquals(), new Empty(),
+                    GetEquals2(), new Empty(),
+                    GetHashCode(), new Empty(),
+                    GetCompare()
                 }
             };
             Members.Elements.Add(methodSection);
@@ -382,12 +163,13 @@ namespace Rusty.Quantities.Generator
         {
             bool mustCast = Types.MustCast("double", type);
 
+            bool @implicit = !mustCast || type == "float" || type == "string";
             string implementation = $"return {Types.Convert("value", Name, type)};";
 
             return new CastingOperator()
             {
                 Summary = $"Convert this {LowercaseName} quantity to {Types.Adjectives[type]} {type} object.",
-                Modifier = mustCast ? CastingModifierID.Explicit : CastingModifierID.Implicit,
+                Modifier = @implicit ? CastingModifierID.Implicit : CastingModifierID.Explicit,
                 ReturnType = type,
                 Operand = new Parameter(Name, "value"),
                 Implementation = implementation
@@ -403,6 +185,64 @@ namespace Rusty.Quantities.Generator
                 Modifiers = MethodModifierID.Override,
                 ReturnType = "string",
                 Implementation = "return value.ToString();",
+            };
+        }
+
+        public Method GetEquals()
+        {
+            return new Method()
+            {
+                Summary = $"Check if this {Name} object is equal to another object.",
+                Modifiers = MethodModifierID.Override,
+                ReturnType = "bool",
+                Name = "Equals",
+                Parameters = new Parameter("object", "obj"),
+                Implementation = $"return obj is {Name} value && Equals(value);"
+            };
+        }
+
+        public Method GetEquals2()
+        {
+            return new Method()
+            {
+                Summary = $"Check if this {Name} object is equal to another {Name} object.",
+                ReturnType = "bool",
+                Name = "Equals",
+                Parameters = new Parameter(Name, "value"),
+                Implementation = $"return this.value == value.value;"
+            };
+        }
+
+        public Method GetHashCode()
+        {
+            return new Method()
+            {
+                Summary = $"Get a hash code for this object.",
+                Modifiers = MethodModifierID.Override,
+                ReturnType = "int",
+                Name = "GetHashCode",
+                Implementation = $"return value.GetHashCode();"
+            };
+        }
+
+        public Method GetCompare()
+        {
+            string implementation =
+                $"if (this > other)" +
+                $"\n    return 1;" +
+                $"\nelse if (this < other)" +
+                $"\n    return -1;" +
+                $"\nelse" +
+                $"\n    return 0;";
+
+            return new Method()
+            {
+                Summary = $"Compare two {Name} objects.",
+                Modifiers = MethodModifierID.Readonly,
+                Name = "CompareTo",
+                ReturnType = "int",
+                Parameters = new Parameter(Name, "other"),
+                Implementation = implementation
             };
         }
     }
